a732ddfa11173c754674c0483ce2739e9a1982d3
DanielEspinalGil <danieles095@outlook.es>
2024-11-15 18:26:14 -0500

    Definición de la API KEY en el archivo vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // define: {
-  //   'process.env.VITE_API_KEY': JSON.stringify('e99d57bf93b9887bbc4342f795766e94'),
-  // },
+  define: {
+    'process.env.VITE_API_KEY': JSON.stringify('e99d57bf93b9887bbc4342f795766e94'),
+  },
   base: 'Weather_App',
 })
