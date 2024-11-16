467f3a9f8427a4d882b2ebd65245a669af4efbb7
DanielEspinalGil <danieles095@outlook.es>
2024-11-15 23:29:17 -0500

    Prueba 9

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/Weather_App/',
+  server: {
+    fs: {
+      allow: ['..'], // Asegúrate de que Vite pueda acceder a los archivos correctamente
+    },
+  },
 })
